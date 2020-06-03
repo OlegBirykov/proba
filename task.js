4fbaff1213536b4f59061a5369103c81c713c06d
OlegBirykov <59805563+OlegBirykov@users.noreply.github.com>
2020-06-03 20:40:56 +0300

    Update task.js

--- task.js
+++ task.js
@@ -4,19 +4,16 @@ const modalWindow = document.getElementById('subscribe-modal');
 const modalClose = modalWindow.querySelector('.modal__close');
 
 const getCookie = name => {
-  const value = "; " + document.cookie;
-  let parts = value.split("; " + name + "=");
+  const value = '; ' + document.cookie;
+  let parts = value.split('; ' + name + '=');
   if (parts.length === 2)
-    return parts.pop().split(";").shift();
+    return parts.pop().split(';').shift();
 }
 
 modalClose.addEventListener('click', () => {
   modalWindow.classList.remove('modal_active');
   document.cookie = 'modal=close; Max-Age=31536000';
-  console.log(document.cookie);
 });
-
-console.log(getCookie('modal'));
 
 if (getCookie('modal') !== 'close')
   modalWindow.classList.add('modal_active');
