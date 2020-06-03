629ef154dca4ebfa5a132d4ea711ee98ec874d77
OlegBirykov <59805563+OlegBirykov@users.noreply.github.com>
2020-06-03 19:53:49 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modalWindow = document.getElementById('subscribe-modal');
+const modalClose = modalWindow.querySelector('.modal__close');
+
+modalClose.addEventListener('click', () => {
+  modalWindow.classList.remove('modal_active');
+  document.cookie = 'modal=close';
+  console.log(document.cookie);
+});
+
+if (document.cookie !== 'modal=close')
+  modalWindow.classList.add('modal_active');
